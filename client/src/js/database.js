f4cd881755c5bba05cb68d5c0c2c0fa050f679e7
The-Schwiz <113313870+The-Schwiz@users.noreply.github.com>
2023-03-19 15:46:17 -0400

    update db, install, src-sw

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,23 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  const jateDb = await initdb();
+  const tx = jateDb.transaction('jate', 'readwrite') 
+  const store = tx.objectStore('jate')
+  const request = store.put({id: 1, value: content})
+  const result = await request;
+  console.log(':rocket: - data saved to the datbase ', result.value);
+}
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await initdb();
+  const tx = jateDb.transaction('jate', 'readonly')
+  const store = tx.objectStore('jate')
+  const request = store.get(1)
+  const result = await request;
+  result ? console.log(':rocket: - data saved to the datbase ', result.value) : console.log(':rocket: - data not found in the datbase');
+  return result?.value;
+} 
 
-initdb();
+// initdb();
